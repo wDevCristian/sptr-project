9d60ab7e1386e5753701fe4540d3585b3bb4a331
Veniamin <veniamin.balan02@gmail.com>
2025-06-07 21:32:31 +0300

    sensor read

--- sketch.ino
+++ sketch.ino
@@ -1,7 +1,11 @@
+#include <LiquidCrystal_I2C.h>
 #include <Arduino_FreeRTOS.h>
 #include <DHT.h>
-#include <Wire.h>
-#include <LiquidCrystal_I2C_STEM.h>
+#include "queue.h"
+
+#define I2C_ADDR    0x27
+#define LCD_COLUMNS 20
+#define LCD_LINES   4
 
 // --- Configurare senzori ---
 #define DHTPIN 2
@@ -9,103 +13,99 @@
 #define MQ2PIN A0
 
 // --- Butoane ---
-#define BUTTON_UP 3
-#define BUTTON_DOWN 4
+#define BUTTON_UP 27
+#define BUTTON_DOWN 29
+
+#define GasThreshold 400
 
 DHT dht(DHTPIN, DHTTYPE);
-LiquidCrystal_I2C_STEM lcd(0x27, 20, 4);  // LCD I2C address
+LiquidCrystal_I2C lcd(I2C_ADDR, LCD_COLUMNS, LCD_LINES);
 
-// Structura pentru stocarea datelor senzorilor
 typedef struct {
   float temperature;
   float humidity;
-  int co2ppm;
+  float heatIndex;
+  unsigned int co2ppm;
 } AirData;
 
-AirData airData; // global shared data
+QueueHandle_t queueSensorData;
 
-// Handle-uri pentru taskuri
-TaskHandle_t monitorTaskHandle = NULL;
-TaskHandle_t lcdTaskHandle = NULL;
+TaskHandle_t TaskHandleLCD;
+TaskHandle_t TaskHandleReadSensors;
 
-// --- Conversie simulată CO2 ---
-int analogToCO2ppm(int analogValue) {
-  return map(analogValue, 0, 1023, 200, 10000);
-}
-
-// --- Task: Monitorizare senzori ---
-void TaskMonitorAir(void *pvParameters) {
-  (void) pvParameters;
-
-  for (;;) {
-    float t = dht.readTemperature();
-    float h = dht.readHumidity();
-    int co2 = analogRead(MQ2PIN);
-    co2 = analogToCO2ppm(co2);
-
-    if (!isnan(t) && !isnan(h)) {
-      // Protejăm accesul la datele globale
-      taskENTER_CRITICAL();
-      airData.temperature = t;
-      airData.humidity = h;
-      airData.co2ppm = co2;
-      taskEXIT_CRITICAL();
-    }
-
-    vTaskDelay(pdMS_TO_TICKS(2000));
-  }
-}
-
-// --- Task: Afișare LCD ---
-void TaskDisplayLCD(void *pvParameters) {
-  (void) pvParameters;
-
-  float temp, hum;
-  int co2;
-
-  for (;;) {
-    // Copiem datele protejat
-    taskENTER_CRITICAL();
-    temp = airData.temperature;
-    hum = airData.humidity;
-    co2 = airData.co2ppm;
-    taskEXIT_CRITICAL();
-
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Temp: ");
-    lcd.print(temp, 1);
-    lcd.print(" C");
-
-    lcd.setCursor(0, 1);
-    lcd.print("Umid: ");
-    lcd.print(hum, 1);
-    lcd.print(" %");
-
-    lcd.setCursor(0, 2);
-    lcd.print("CO2: ");
-    lcd.print(co2);
-    lcd.print(" ppm");
-
-    vTaskDelay(pdMS_TO_TICKS(2000));
-  }
-}
+int readInterval = 2000;
 
 void setup() {
   pinMode(BUTTON_UP, INPUT_PULLUP);
   pinMode(BUTTON_DOWN, INPUT_PULLUP);
 
   Serial.begin(9600);
-  //Wire.begin();       // Necesare pentru I2C
+
+  queueSensorData = xQueueCreate(5, sizeof(AirData));
+
+  xTaskCreate(TaskLCD, "LCD", 128, NULL, 1, &TaskHandleLCD);
+  xTaskCreate(TaskReadSensors, "ReadSensors", 128, NULL, 2, &TaskHandleReadSensors);
+
+}
+
+void TaskReadSensors(void *pvParameters) {
+  AirData data;
   dht.begin();
-  lcd.init();         // Moved here from task
+  for (;;) {
+    // float h = dht.readHumidity();
+    // float t = dht.readTemperature();
+
+    // if (isnan(h) || isnan(t)) {
+    //   Serial.println(F("Failed to read from DHT sensor!"));
+    // }
+
+    //data.humidity = h;
+    //data.temperature = t;
+    //data.heatIndex = dht.computeHeatIndex(t, h, false);
+
+    data.co2ppm = analogRead(MQ2PIN); 
+
+    xQueueSend(queueSensorData, &data, 0);
+    
+    vTaskDelay(pdMS_TO_TICKS(readInterval));
+  }
+}
+
+
+void TaskLCD(void *pvParameters){
+  lcd.init();
   lcd.backlight();
+  uint8_t lcdIndex=11;
 
-  // Creăm taskurile cu stack mai mare
-  xTaskCreate(TaskMonitorAir, "Monitor", 384, NULL, 2, &monitorTaskHandle);
-  xTaskCreate(TaskDisplayLCD, "Display", 384, NULL, 1, &lcdTaskHandle);
+  for(;;){
+    AirData data;
+
+    if (xQueueReceive(queueSensorData, &data, portMAX_DELAY) == pdTRUE)
+    {
+      lcd.setCursor(0, 0);
+      lcd.print("Temp: ");
+      lcd.print(data.temperature);
+      lcd.print(" C  ");
+
+      lcd.setCursor(0, 1);
+      lcd.print("Hum:  ");
+      lcd.print(data.humidity);
+      lcd.print(" %   ");
+
+      lcd.setCursor(0, 2);
+      if (data.co2ppm > GasThreshold)
+      {
+        lcd.print("Gas: High Level!");
+      }else
+      {
+        lcd.print("Gas: Safe Level");
+      }
+
+      lcd.print("     ");
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(100));
+  }
 }
 
-void loop() {
-  // Nu este folosit - FreeRTOS rulează taskurile
-}
+void loop() {}
